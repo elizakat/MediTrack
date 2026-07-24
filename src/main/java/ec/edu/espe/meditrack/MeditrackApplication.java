97df416925ad99a971ae7c9c284bbd856d6a33c7
Carlos Licto <carlosels.dev@gmail.com>
2026-07-23 22:30:57 -0500

    Commit inicial con configuracion del proyecto base

---
+++ src/main/java/ec/edu/espe/meditrack/MeditrackApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.meditrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditrackApplication.class, args);
+	}
+
+}
